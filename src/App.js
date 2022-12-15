6c4afa5aa32c668ce6087e201f9d45ae7a47e102
LongHaHoang <longhahoang1992@gmail.com>
2022-12-15 23:12:48 +0100

    add: footer to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./componet/Header";
+import Footer from "./componet/Footer";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
