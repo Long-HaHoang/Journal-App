5ce4dcb046a004bfc788f508ea3be21f5de5a31f
LongHaHoang <longhahoang1992@gmail.com>
2022-12-16 00:28:02 +0100

    add EntryForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Header from "./componet/Header";
 import Footer from "./componet/Footer";
+import EntryForm from "./componet/EntryForm";
 
 function App() {
   return (
     <div className="app">
       <Header></Header>
+      <main className="app__main">
+        <EntryForm></EntryForm>
+      </main>
       <Footer></Footer>
     </div>
   );
